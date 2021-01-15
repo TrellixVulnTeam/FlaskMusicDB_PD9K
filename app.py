4c5e433142242d8e4fa0eb39ca19433f8f2744cf
Mohan_Krishna <mohanpavansh@gmail.com>
2021-01-15 11:27:07 +0530

    pre final

--- app.py
+++ app.py
@@ -34,10 +34,11 @@ def hello_world():
 def wildcard_result():
     response = []
     if request.method == "POST":
-        wildcard_data = request.form
-        print(wildcard_data["wild-data"])
+        wildcard_data = request.form["wild-data"]
+        wildcard_data = wildcard_data.upper()
+        print(wildcard_data)
         # query artists for match
-        artists = query_db('SELECT stageName, genre1, genre2, idThumbNail FROM ARTISTS WHERE stageName = ?', [wildcard_data["wild-data"]])
+        artists = query_db('SELECT stageName, genre1, genre2, idThumbNail FROM ARTISTS WHERE UPPER(stageName) LIKE ?', ['%' + wildcard_data + '%'])
         print(artists)
         if artists is not None:
             for artist in artists:
@@ -49,28 +50,53 @@ def wildcard_result():
                 print("result ", result)
                 response.append(result)
         # query album for match
-        albums = query_db("SELECT name, year, thumbnail FROM ALBUMS WHERE name = ?", [wildcard_data["wild-data"]])
+        albums = query_db("SELECT name, year, thumbnail FROM ALBUMS WHERE UPPER(name) LIKE ?", ['%' + wildcard_data + '%'])
         print(albums)
         if albums is not None:
             for album in albums:
                 result = dict()
                 thumbnail = query_db('SELECT filename from THUMBNAIL WHERE idThumbNail = ?', [album['idThumbNail']], one=True)
                 result["thumbnail"] = thumbnail['filename']
-                result["name"]: album['name']
-                result["details"]: album['year']
+                result["name"] = album['name']
+                result["details"] = album['year']
                 response.append(result)
         # query tracks for match
-        tracks = query_db("SELECT trackName, genre1, genre2, year, idthumbnail FROM TRACKS WHERE trackName = ?", [wildcard_data["wild-data"]])
+        tracks = query_db("SELECT T.trackName, T.genre1, T.genre2, T.year, T.idthumbnail FROM TRACKS AS T, ARTISTS AS A WHERE UPPER(T.trackName) = ? OR (UPPER(A.stageName) LIKE ? AND T.idArtist = A.idArtist)" , ['%' + wildcard_data + '%', '%' + wildcard_data + '%'])
         print(tracks)
         if tracks is not None:
             for track in tracks:
                 result = dict()
-                thumbnail = query_db('SELECT filename from THUMBNAIL WHERE idThumbNail = ?', [track['idThumbNail']],
+                thumbnail = query_db('SELECT filename from THUMBNAIL WHERE idThumbNail = ?', [track['idthumbnail']],
                                      one=True)
                 result["thumbnail"] = thumbnail['filename']
-                result["name"]: track['trackName']
-                result["details"]: track['genre1'] + "/" + track['genre1'] + " " + track['year']
+                result["name"] = track['trackName']
+                result["details"] = track['genre1'] + "/" + track['genre1'] + "" + str(track['year'])
+                print(result)
                 response.append(result)
+        print(response)
+    return render_template('response.html', response=response)
+
+
+@app.route('/advanced_result', methods=['POST', 'GET'])
+def advanced_result():
+    response = []
+    if request.method == "POST":
+        wildcard_data = request.form
+        print(wildcard_data)
+        song_name = wildcard_data['song_name']
+        artist_name = wildcard_data['artist_name']
+        album_name = wildcard_data['album_name']
+        lyrics = wildcard_data['lyrics']
+        query = "SELECT T.trackName, T.genre1, T.genre2, T.year, T.idthumbnail FROM TRACKS AS T, ARTISTS AS AR, ALBUMS AS AL WHERE UPPER(T.lyrics) LIKE %?% AND UPPER(T.trackName) LIKE %?% AND UPPER(AL.name) LIKE %?% AND UPPER(AR.stageName) LIKE %?% AND AL.idAlbum=T.idAlbum AND AR.idArtist=T.idArtist"
+        track = query_db(query, [lyrics.upper(), song_name.upper(), album_name.upper(), artist_name.upper()], one=True)
+        if track is not None:
+            result = dict()
+            thumbnail = query_db('SELECT filename from THUMBNAIL WHERE idThumbNail = ?', [track['idThumbNail']],
+                                 one=True)
+            result["thumbnail"] = thumbnail['filename']
+            result["name"] = track['trackName']
+            result["details"] = track['genre1'] + "/" + track['genre1'] + "" + str(track['year'])
+            response.append(result)
         print(response)
     return render_template('response.html', response=response)
 
